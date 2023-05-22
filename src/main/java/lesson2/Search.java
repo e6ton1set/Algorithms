2d0753f11eda329fa7c10d3fca9e1f03c30f0aa1
Ruslan Karimov <ru-ka-@mail.ru>
2023-05-22 21:05:06 +0500

    added method binarySearch (бинарный поиск) where O(log n)

--- src/main/java/lesson2/Search.java
+++ src/main/java/lesson2/Search.java
@@ -6,7 +6,8 @@ package lesson2;
 public class Search {
     public static void main(String[] args) {
         int [] array = new int[]{1, 10, 20, 25, 13};
-        System.out.println(simpleFind(array, 10));
+        //System.out.println(simpleFind(array, 10));
+        System.out.println(binarySearch(array, 25, 0, array.length-1));
     }
     public static int simpleFind(int [] array, int value){
         for (int i = 0; i < array.length; i++) {
@@ -15,5 +16,27 @@ public class Search {
             }
         }
         return -1;
+    }
+
+// Алгоритм бинарного поиска - по половинам от min до max
+// Сложность O(log n), т.к. для добавления 1 доп. итерации требуется
+// увеличить исходные данные в 2 раза
+// ИТОГОВАЯ СЛОЖНОСТЬ: O(log n)
+    public static int binarySearch(int [] array, int value, int min, int max){
+        int midpoint;
+        if (max < min){
+            return -1;
+        } else {
+            midpoint = (max - min) / 2 + min;
+        }
+        if (array[midpoint]< value){
+            return binarySearch(array, value, midpoint +1, max);
+        } else {
+            if (array[midpoint] > value){
+                return binarySearch(array, value, min, midpoint-1);
+            } else {
+                return midpoint;
+            }
+        }
     }
 }
