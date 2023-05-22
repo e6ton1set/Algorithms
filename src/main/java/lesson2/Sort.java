668b3c9dfa37afc46a2a1d14d178c49704f4cf7e
Ruslan Karimov <ru-ka-@mail.ru>
2023-05-22 20:20:14 +0500

    added method insertSort (сортировка вставками) where O(n^2)

--- src/main/java/lesson2/Sort.java
+++ src/main/java/lesson2/Sort.java
@@ -53,4 +53,18 @@ public class Sort {
             }
         }
     }
+// Поскольку мы имеем цикла - основной и вложенный -
+// сложность также будет O(n^2)
+// ИТОГОВАЯ СЛОЖНОСТЬ: O(n^2)
+    public static void insertSort (int [] array){
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if(array[i] > array[j]){
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
 }
