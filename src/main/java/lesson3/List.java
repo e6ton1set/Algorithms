8b2db5aa0fa63c644dd89c313004b807fc7bd468
Ruslan Karimov <ru-ka-@mail.ru>
2023-05-25 15:41:47 +0500

    added twoConnectedList where O(n). при этом структуру List лучше использовать, когда нужно вставить элемент в начало или в конец. скорость выполнения будет выше, чем у массива

--- src/main/java/lesson3/List.java
+++ src/main/java/lesson3/List.java
@@ -3,8 +3,38 @@ package lesson3;
 public class List {
     // односвязный список - хранит значение и ссылку на следующий элемент
     Node head; // описывает начало списка
+    Node tail; // описывает конец списка
+
+    // метод добавления по двусвязному списку
+    public void add(int value){
+        Node node = new Node();
+        node.value = value;
+        if(head==null){
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            node.previous = tail;
+            tail = node;
+        }
+    }
+    public void add (int value, Node node){
+        Node next = node.next;
+        Node newNode = new Node();
+        newNode.value = value;
+        node.next = newNode;
+        newNode.previous = tail;
+        tail = node;
+        if (next == null){
+            tail = newNode;
+        } else {
+            next.previous = newNode;
+            newNode.next = next;
+        }
+    }
 
     // сложность перебора O(n)
+    // метод поиска по дносвязному списку
     public Node find(int value){
         Node currentNode = head;
         while (currentNode != null){
@@ -18,5 +48,6 @@ public class List {
     public class Node{
         int value;
         Node next;
+        Node previous;
     }
 }
