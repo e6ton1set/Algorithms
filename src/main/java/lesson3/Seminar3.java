232f31834a23def468fb0a284fe03f08342a3f65
Ruslan Karimov <ru-ka-@mail.ru>
2023-05-26 18:52:25 +0500

    реализуем простой односвязный список.

---
+++ src/main/java/lesson3/Seminar3.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+public class Seminar3 {
+    // реализуем простой односвязный список.
+    // пишем только структуру без методов.
+
+    private Node head;
+    private class Node{
+        private int value;
+        private Node next;
+    }
+}
