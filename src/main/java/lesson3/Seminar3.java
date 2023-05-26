a08b0567403e97682ddd596b16775e85eb09b687
Ruslan Karimov <ru-ka-@mail.ru>
2023-05-26 19:18:37 +0500

    реализуем метод добавления новых элементов в начало списка и удаления первого элемента

--- src/main/java/lesson3/Seminar3.java
+++ src/main/java/lesson3/Seminar3.java
@@ -3,10 +3,29 @@ package lesson3;
 public class Seminar3 {
     // реализуем простой односвязный список.
     // пишем только структуру без методов.
-
     private Node head;
-    private class Node{
+    private class Node {
         private int value;
         private Node next;
     }
+
+    // реализуем метод добавления новых элементов в начало списка.
+    public void addFirst(int value) {
+        Node first = new Node();
+        first.value = value;
+        if (head != null) {
+            first.next = head;
+        }
+        head = first;
+    }
+
+    // реализуем метод удаления первого элемента связанного списка.
+    public void deleteFirst(){
+        if(head != null){
+            head = head.next;
+        }
+    }
+
+
+
 }
