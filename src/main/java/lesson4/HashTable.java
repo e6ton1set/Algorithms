1ad8e618dedc1e2c65cd870d6473b32f1bb4a8de
Ruslan Karimov <ru-ka-@mail.ru>
2023-06-01 16:07:24 +0500

    added task2

--- src/main/java/lesson4/HashTable.java
+++ src/main/java/lesson4/HashTable.java
@@ -1,17 +1,28 @@
 package lesson4;
 
 import lesson3.List;
-
 import java.util.LinkedList;
 
-public class HashTable {
-    class Hash <K,V>{
+public class HashTable<K,V> {
+        private static final int init_basket_count = 16;
+        private Basket[] baskets;
+
+        public HashTable(){
+            this(init_basket_count);
+        }
+        public HashTable(int initSize){
+            baskets = (Basket[]) new Object[initSize];
+        }
+
+        private int CalculateBasketIndex(K key){
+            return key.hashCode()%baskets.length;
+        }
         private class Entity{
             private K key;
             private V value;
         }
 
-        private class basket{
+        private class Basket{
             private Node head;
             private class Node{
                 private Node next;
@@ -19,4 +30,3 @@ public class HashTable {
             }
         }
     }
-}
