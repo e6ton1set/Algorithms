1bc3051ebfc4880514fe68b50a7a1b9b4b7ddb96
Ruslan Karimov <ru-ka-@mail.ru>
2023-06-01 15:52:28 +0500

    added structure

--- src/main/java/lesson4/HashTable.java
+++ src/main/java/lesson4/HashTable.java
@@ -1,4 +1,22 @@
 package lesson4;
 
+import lesson3.List;
+
+import java.util.LinkedList;
+
 public class HashTable {
+    class Hash <K,V>{
+        private class Entity{
+            private K key;
+            private V value;
+        }
+
+        private class basket{
+            private Node head;
+            private class Node{
+                private Node next;
+                private Entity value;
+            }
+        }
+    }
 }
