5900e83659922ff576c762bbdfcadb8e3c1e5488
Ruslan Karimov <ru-ka-@mail.ru>
2023-06-01 17:34:34 +0500

    added homework4

--- src/main/java/lesson4/Main.java
+++ src/main/java/lesson4/Main.java
@@ -1,4 +1,31 @@
 package lesson4;
 
 public class Main {
+    public static void main(String[] args) {
+        RBTrees node = new RBTrees();
+
+        node.insert(20);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(40);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(50);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(-44);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(-66);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(-22);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(-21);
+        node.inorder();
+        System.out.println("\n");
+        node.insert(-2005);
+        node.inorder();
+    }
 }
