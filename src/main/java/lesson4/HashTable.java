ccbd39521e406b0e0892bf1c59897eae8470f446
Ruslan Karimov <ru-ka-@mail.ru>
2023-06-01 17:21:30 +0500

    added methods put and remove

--- src/main/java/lesson4/HashTable.java
+++ src/main/java/lesson4/HashTable.java
@@ -1,56 +1,96 @@
 package lesson4;
 
 import lesson3.List;
+
 import java.util.LinkedList;
 
-public class HashTable<K,V> {
-        private static final int init_basket_count = 16;
-        private Basket[] baskets;
+public class HashTable<K, V> {
+    private static final int init_basket_count = 16;
+    private Basket[] baskets;
 
-        public HashTable(){
-            this(init_basket_count);
+    public HashTable() {
+        this(init_basket_count);
+    }
+
+    public HashTable(int initSize) {
+        baskets = (Basket[]) new Object[initSize];
+    }
+
+    private int calculateBasketIndex(K key) {
+        return key.hashCode() % baskets.length;
+    }
+
+
+    private class Entity {
+        private K key;
+        private V value;
+    }
+
+    private class Basket {
+        private Node head;
+
+        private class Node {
+            private Node next;
+            private Entity value;
         }
-        public HashTable(int initSize){
-            baskets = (Basket[]) new Object[initSize];
-        }
 
-        private int calculateBasketIndex(K key){
-            return key.hashCode()%baskets.length;
-        }
-
-
-        private class Entity{
-            private K key;
-            private V value;
-        }
-
-        private class Basket{
-            private Node head;
-            private class Node{
-                private Node next;
-                private Entity value;
+        public V getBasket(K key) {
+            Node item = head;
+            while (item != null) {
+                if (item.value.key.equals(key)) {
+                    return item.value.value;
+                }
+                item = item.next;
             }
+            return null;
+        }
 
-            public V getBasket(K key){
-                Node item = head;
-                while (item != null){
-                    if(item.value.key.equals(key)){
-                        return item.value.value;
-                     }
+        public V getBasketOut(K key) {
+            int index = calculateBasketIndex(key);
+            Basket basket = baskets[index];
+            if (basket != null) {
+                return basket.getBasket(key);
+            }
+            return null;
+        }
+
+        public boolean put(Entity entity) {
+            Node node = new Node();
+            node.value = entity;
+            Node item = head;
+            if (item != null) {
+                while (true) {
+                    if (item.value.key.equals(entity.key)) {
+                        return false;
+                    }
+                    if (item.next == (null)) {
+                        item.next = node;
+                        return true;
+                    }
                     item = item.next;
                 }
-                return null;
             }
+            head = node;
+            return true;
+        }
 
-            public V getBasketOut(K key){
-               int index = calculateBasketIndex(key);
-               Basket basket = baskets[index];
-               if (basket != null){
-                  return basket.getBasket(key);
-               }
-               return null;
+        public boolean remove(K key) {
+            if (head != null) {
+                if (head.value.key.equals(key)) {
+                    head = head.next;
+                    return true;
+                } else {
+                    Node node = head;
+                    while (node.next != null) {
+                        if (node.next.value.key.equals(key)) {
+                            node.next = node.next.next;
+                            return true;
+                        }
+                        node = node.next;
+                    }
+                }
             }
-
-
+            return false;
         }
     }
+}
